c63e8843058110ab4519c6556f79d96131607a5e
Raixther <81520277+Raixther@users.noreply.github.com>
2022-01-08 15:13:59 +0300

    Add files via upload

---
+++ Lesson_1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson_1.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class WeatherForecastController : ControllerBase
+	{
+		private static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+
+		private readonly ILogger<WeatherForecastController> _logger;
+
+		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IEnumerable<WeatherForecast> Get(DateTime date1, DateTime date2)
+		{
+			return Enumerable.AsEnumerable<WeatherForecast>(WeatherForecastData.data.FindAll(x=>x.Date>date1||x.Date<date2));
+			
+		}
+
+		[HttpPut]
+		public void Put(DateTime date, int temperature, string summary)
+		{
+			WeatherForecast forecast = new WeatherForecast();
+			forecast.Date = date;
+			forecast.TemperatureC = temperature;
+			forecast.Summary = summary;		
+			WeatherForecastData.data.Add(forecast);
+		}
+		[HttpPut]
+		public void Update(DateTime date, int temperature)
+		{
+			
+			 WeatherForecastData.data.Find(x => x.Date == date).TemperatureC = temperature;
+
+		}
+		[HttpDelete]
+		public void Delete(DateTime date1, DateTime date2)
+		{
+			WeatherForecastData.data.RemoveAll(x => x.Date > date1 && x.Date < date2);
+		}
+
+	}
+}
