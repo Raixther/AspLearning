9c2c86123e25e433f19bdd782a62acad936de738
Raixther <81520277+Raixther@users.noreply.github.com>
2022-01-17 18:03:42 +0300

    Add files via upload

---
+++ RAMMetricsControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using MetricsManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using MetricsManager.Models;
+using Moq;
+using MetricsManager;
+using Microsoft.Extensions.Logging;
+
+namespace MetricsManagerTests
+{
+	public class RAMMetricsControllerTests
+	{
+
+		private Mock<ILogger<RAMMetricsController>> _loggerMock;
+
+		private Mock<IMetricsRepository<RAMMetric>> _RAMMetricsRepositoryMock;
+
+		public RAMMetricsControllerTests()
+		{
+			_loggerMock = new Mock<ILogger<RAMMetricsController>>();
+
+			_RAMMetricsRepositoryMock = new Mock<IMetricsRepository<RAMMetric>>();
+		}
+
+
+
+		[Fact]
+		public void GetMetricsFromAgent_ReturnsOk()
+		{
+			var controller = new RAMMetricsController(_loggerMock.Object, _RAMMetricsRepositoryMock.Object);
+
+			var result = controller.GetMetricsFromAgent(1, TimeSpan.Zero, TimeSpan.Zero);
+
+			Assert.IsAssignableFrom<IActionResult>(result);
+
+		}
+
+	}
+}
